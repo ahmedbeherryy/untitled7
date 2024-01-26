373b66328c0c48469df876e7b01da53b238e2f5d
SOFT AMR <ahmedbeherry9@gmail.com>
2024-01-26 23:30:14 +0200

    new

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp( WhatsApp());
+}
+
+class WhatsApp extends StatelessWidget {
+  const WhatsApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.green,
+            leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+            title: Text(
+              'WhatsApp',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+            ],
+            bottom: TabBar(
+                labelStyle: TextStyle(
+                  fontSize: 18,
+                ),
+                tabs: [
+                  Tab(
+                    text: 'CHAT',
+                  ),
+                  Tab(
+                    text: 'STATUS',
+                  ),
+                  Tab(
+                    text: 'CALLS',
+                  )
+                ]),
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.ac_unit,
+                      size: 60,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Mohamed sayed',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 90),
+                          child: Text(
+                            'hello my friend...',
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.ac_unit,
+                      size: 60,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Ronaldo',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'حجز الساعة 5 جاي ؟',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.ac_unit,
+                      size: 60,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 30),
+                          child: Text(
+                            'John',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(
+                          'where are you ?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, top: 3),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.ac_unit,
+                      size: 60,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'سرج',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'يا انستراكتور',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
